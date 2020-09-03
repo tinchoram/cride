b13468593dc45ed44b299871017f3d726de2709a
Tinchoram <tinchoram@gmail.com>
2020-09-03 17:39:32 -0300

    update

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -70,8 +70,8 @@ class UserSignUpSerializer(serializers.Serializer):
     def create(self, data):
         """Handle user and profile creation"""
         data.pop('password_confirmation')
-        user = User.objects.create_user(**data)
-        profile = Profile.objects.create(user=user)
+        user = User.objects.create_user(**data, is_verified=False)
+        Profile.objects.create(user=user)
         return user
 
 
@@ -88,6 +88,8 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Invalid credentials')
+        if not user.is_verified:
+            raise serializers.ValidationError('Account is not active yet :(')
         self.context['user'] = user
         return data
 
